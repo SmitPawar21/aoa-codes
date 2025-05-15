a1181e7a2836cbadc6d08df80186582c558f6880
Smit Pawar <dynaster21@gmail.com>
2025-05-15 21:05:59 +0530

    quick-sort gpt'ed

--- quicksort.c
+++ quicksort.c
@@ -23,7 +23,7 @@ int partition(int arr[], int low, int high) {
 
 void quickSort(int arr[], int low, int high) {
     if (low < high) {
-        int pi = partition(arr, low, high); /
+        int pi = partition(arr, low, high); 
 
         quickSort(arr, low, pi - 1); 
         quickSort(arr, pi + 1, high); 
